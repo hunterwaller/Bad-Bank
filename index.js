9daf80144fd425c5f28bdb166441410b4c12a4dd
HunterW03 <hawaller03@gmail.com>
2022-08-11 11:44:18 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app     = express();
+var cors    = require('cors');
+var dal     = require('./dal.js')
+
+// used to serve static files from the public directory
+app.use(express.static('public'));
+app.use(cors());
+
+// create user account
+app.get('/account/create/:name/:email/:password', function (req, res) {
+  // else create user
+  dal.create(req.params.name, req.params.email, req.params.password)
+    .then((user) => {
+      console.log(user);
+      res.send(user);
+    });
+});
+
+// all accounts
+app.get('/account/all', function(req, res) {
+  dal.all()
+    .then((docs) => {
+      console.log(docs);
+      res.send(docs);
+    });
+});
+
+app.get('/account/deposit/:email/:amount', function (req, res) {
+  res.send({
+    email: req.params.email,
+    amount: req.params.amount
+  });
+});
+
+app.get('/account/withdraw/:email/:amount', function (req, res) {
+  res.send({
+    email: req.params.email,
+    amount: req.params.amount
+  });
+});
+
+app.get('/account/deposit/:email/:balance', function (req, res) {
+  res.send({
+    email: req.params.email,
+    amount: req.params.balance
+  });
+});
+
+var port = 3000;
+app.listen(port);
+console.log('Running on port: ' + port);
